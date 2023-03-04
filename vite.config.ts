a53c37f19624a011a2dae4ce3b9e1469929a37b1
Nicolai <nicolai.verbaarschot@gmail.com>
2023-03-04 19:44:45 +0100

    attempt at mediapipe rollup fix.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import * as fs from "fs";
+import * as path from "path";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), mediapipe_workaround()],
   ssr: {
     noExternal: ["three"],
   },
 });
+
+function mediapipe_workaround() {
+  return {
+    name: "mediapipe_workaround",
+    load(id: string) {
+      if (path.basename(id) === "face_mesh.js") {
+        let code = fs.readFileSync(id, "utf-8");
+        code += "exports.FaceMesh = FaceMesh;";
+        return { code };
+      } else {
+        return null;
+      }
+    },
+  };
+}
